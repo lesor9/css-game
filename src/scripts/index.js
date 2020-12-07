8c83afc8b83cd7c11bfa9c8636976e0fd164e15b
lesor9 <lesor@protonmail.coml>
2020-12-08 01:29:27 +0300

    feat: add footer and import highlightjs library

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,30 @@
 import '../css/reset.css';
 import '../css/style.css';
+import '../css/dracula.css';
+
+const burgerOpen = () => {
+    document.body.classList.toggle('burger-open');
+};
+
+hljs.configure({languages: ['css']});
+const cssChanged = () => {
+    hljs.highlightBlock(cssEditBlock);
+}
 
 const burgerBtn = document.querySelector('.burger-btn');
-burgerBtn.addEventListener('click', () => {
-    burgerBtn.classList.toggle('burger-open');
+burgerBtn.addEventListener('click', burgerOpen);
+
+const overlay = document.querySelector('.overlay');
+overlay.addEventListener('click', burgerOpen);
+
+const cssEditBlock = document.querySelector('.css_game-textarea');
+cssEditBlock.addEventListener('blur', cssChanged);
+cssEditBlock.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        cssChanged();
+        cssEditBlock.blur();
+    }
 });
+
+
+
