5ffa9ee08a872f0fc0575e083f0b55dc63edab39
lesor9 <lesor@protonmail.coml>
2020-12-23 23:03:26 +0300

    refactor: some changes

--- rs-css/src/scripts/controller/checkSelector.js
+++ rs-css/src/scripts/controller/checkSelector.js
@@ -5,9 +5,7 @@ const checkSelector = () => {
   for (let i = 0; i < pseudoCode.length; i += 1) {
     const isElemMarkInHTML = getComputedStyle(pseudoCode[i]).content === '"marked"';
     const isImageShouldMark = images[i].classList.contains('target');
-    if (isElemMarkInHTML !== isImageShouldMark) {
-      return false;
-    }
+    if (isElemMarkInHTML !== isImageShouldMark) return false;
   }
 
   return true;
